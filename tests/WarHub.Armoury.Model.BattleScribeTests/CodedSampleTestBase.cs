4bcfa366274772cb48c60327a62f4f695fc55f1e
Amadeusz Sadowski <amis.tau.92@gmail.com>
2016-03-03 15:50:53 +0100

    initial 0.1.0 BattleScribeTests project files

---
+++ tests/WarHub.Armoury.Model.BattleScribeTests/CodedSampleTestBase.cs
@@ -0,0 +1,38 @@
+﻿// WarHub licenses this file to you under the MIT license.
+// See LICENSE file in the project root for more information.
+
+namespace WarHub.Armoury.Model.BattleScribeTests
+{
+    using BattleScribe.Services;
+
+    public class CodedSampleTestBase
+    {
+        protected ICatalogue Catalogue { get; private set; }
+
+        protected IRoster Roster { get; private set; }
+
+        protected SampleObjectFactory SampleFactory { get; private set; }
+
+        protected IGameSystem System { get; private set; }
+
+        protected void CatalogueInitialization()
+        {
+            Catalogue = SampleFactory.SampleCatalogue;
+        }
+
+        protected void DataToolsInitialization()
+        {
+            SampleFactory = new SampleObjectFactory();
+        }
+
+        protected void RosterInitialization()
+        {
+            Roster = SampleFactory.SampleRoster;
+        }
+
+        protected void SystemInitialization()
+        {
+            System = SampleFactory.SampleGameSystem;
+        }
+    }
+}
---
+++ tests/WarHub.Armoury.Model.BattleScribeTests/SelectionTests.cs
@@ -0,0 +1,39 @@
+﻿// WarHub licenses this file to you under the MIT license.
+// See LICENSE file in the project root for more information.
+
+namespace WarHub.Armoury.Model.BattleScribeTests
+{
+    using System.Linq;
+    using Xunit;
+
+    public class SelectionTests : CodedSampleTestBase
+    {
+        public SelectionTests()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            DataToolsInitialization();
+            SystemInitialization();
+            CatalogueInitialization();
+            RosterInitialization();
+        }
+
+        [Fact]
+        public void SubSelectionsDefaultConfigTest()
+        {
+            var captainEntry = Catalogue.Entries.Single(x => x.Name.Equals("Captain"));
+            var captain = Roster.Forces
+                .First()
+                .CategoryMocks
+                .First(x => x.Name.Equals(captainEntry.CategoryLink.Target.Name))
+                .Selections
+                .First();
+            Assert.NotEqual(captain.Selections.Count, 0);
+            Assert.NotNull(captain.Selections.SingleOrDefault(x => x.Name.Equals("Armour")));
+            Assert.NotNull(captain.Selections.SingleOrDefault(x => x.Name.Equals("Sword")));
+        }
+    }
+}
