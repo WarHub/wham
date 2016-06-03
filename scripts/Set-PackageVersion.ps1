fb02153e16765458c1dfb6fb458071703714f2e8
Amadeusz Sadowski <amis.tau.92@gmail.com>
2016-06-03 02:27:17 +0200

    fixed script and added safety net for branch names

--- scripts/Set-PackageVersion.ps1
+++ scripts/Set-PackageVersion.ps1
@@ -3,6 +3,8 @@ Param(
     [Parameter(Mandatory=$True, Position=0, ValueFromPipeline=$True)]
     [string]$newVersion
 )
+
+# src patching (update version and dependencies)
 foreach ($projfile in (Get-Item src\*\project.json))
 {
     $fullname = $projfile.FullName
@@ -12,8 +14,18 @@ foreach ($projfile in (Get-Item src\*\project.json))
     $contents = $contents -Replace '("WarHub\.Armoury\.Model([^"]*)":\s*)"([^"]*)"', "`$1`"$newVersion-*`""
     [IO.File]::WriteAllText($fullname, $contents)
 }
-# appveyor patching
 
+# tests patching (update dependencies)
+foreach ($projfile in (Get-Item tests\*\project.json))
+{
+    $fullname = $projfile.FullName
+    Write-Verbose "patching version to '$newVersion' in $fullname"
+    $contents = [IO.File]::ReadAllText($fullname)
+    $contents = $contents -Replace '("WarHub\.Armoury\.Model([^"]*)":\s*)"([^"]*)"', "`$1`"$newVersion-*`""
+    [IO.File]::WriteAllText($fullname, $contents)
+}
+
+# appveyor patching
 $fullname = (Get-Item "appveyor.yml").FullName
 Write-Verbose "patching version to '$newVersion' in $fullname"
 $contents = [IO.File]::ReadAllText($fullname)
