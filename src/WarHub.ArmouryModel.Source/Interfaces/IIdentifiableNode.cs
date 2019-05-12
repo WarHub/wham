71a151e2e007707d521385514f434d99c673275c
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-05-12 19:20:09 +0200

    Update Id-, Name-, Publication- containing interfaces

--- src/WarHub.ArmouryModel.Source/Interfaces/IIdentifiableNode.cs
+++ src/WarHub.ArmouryModel.Source/Interfaces/IIdentifiableNode.cs
@@ -10,11 +10,13 @@ namespace WarHub.ArmouryModel.Source
     }
 
     partial class CatalogueBaseNode : IIdentifiableNode { }
-    partial class RosterElementBaseNode : IIdentifiableNode { }
-    partial class EntryBaseNode : IIdentifiableNode { }
+    partial class CatalogueLinkNode : IIdentifiableNode { }
     partial class CharacteristicTypeNode : IIdentifiableNode { }
-    partial class CostTypeNode : IIdentifiableNode { }
     partial class ConstraintNode : IIdentifiableNode { }
+    partial class CostTypeNode : IIdentifiableNode { }
+    partial class EntryBaseNode : IIdentifiableNode { }
     partial class ProfileTypeNode : IIdentifiableNode { }
+    partial class PublicationNode : IIdentifiableNode { }
+    partial class RosterElementBaseNode : IIdentifiableNode { }
     partial class RosterNode : IIdentifiableNode { }
 }
