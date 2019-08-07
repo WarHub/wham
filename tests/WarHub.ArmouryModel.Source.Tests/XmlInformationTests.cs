50ec46821732ece8bac5ecec08f9c9cf2e372d1c
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-08-07 10:30:16 +0200

    Tiny refactor

--- tests/WarHub.ArmouryModel.Source.Tests/XmlInformationTests.cs
+++ tests/WarHub.ArmouryModel.Source.Tests/XmlInformationTests.cs
@@ -4,48 +4,35 @@ using System.Xml.Schema;
 using FluentAssertions;
 using Xunit;
 using System.Linq;
+using static WarHub.ArmouryModel.Source.XmlInformation;
 
 namespace WarHub.ArmouryModel.Source.Tests
 {
     public class XmlInformationTests
     {
         [Theory]
-        [InlineData(XmlInformation.RootElement.Catalogue)]
-        [InlineData(XmlInformation.RootElement.GameSystem)]
-        [InlineData(XmlInformation.RootElement.Roster)]
-        public void Per_element_xsd_is_available(XmlInformation.RootElement rootElement)
+        [MemberData(nameof(XslMigrationVersionData))]
+        public void Per_element_xsl_migration_is_available(RootElement rootElement, BsDataVersion dataVersion)
         {
-            using (var xsdStream = XmlInformation.OpenXsdStream(rootElement))
+            using (var migrationXslStream = OpenMigrationXslStream(rootElement, dataVersion))
+            {
+                migrationXslStream.Should().NotBeNull();
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ThreeRootElements))]
+        public void Per_element_xsd_is_available(RootElement rootElement)
+        {
+            using (var xsdStream = OpenXsdStream(rootElement))
             {
                 xsdStream.Should().NotBeNull();
             }
         }
 
         [Theory]
-        [MemberData(nameof(XslMigrationVersionData))]
-        public void Per_element_xsl_migration_is_available(
-            XmlInformation.RootElement rootElement,
-            XmlInformation.BsDataVersion dataVersion)
-        {
-            using (var migrationXslStream = XmlInformation.OpenMigrationXslStream(rootElement, dataVersion))
-            {
-                migrationXslStream.Should().NotBeNull();
-            }
-        }
-
-        public static IEnumerable<object[]> XslMigrationVersionData()
-        {
-            return
-                from version in XmlInformation.BsDataVersions
-                from element in new[] { XmlInformation.RootElement.GameSystem, XmlInformation.RootElement.Catalogue }
-                select new object[] { element, version };
-        }
-
-        [Theory]
-        [InlineData(XmlInformation.RootElement.Catalogue)]
-        [InlineData(XmlInformation.RootElement.GameSystem)]
-        [InlineData(XmlInformation.RootElement.Roster)]
-        public void Per_element_xsd_has_no_validation_issues(XmlInformation.RootElement rootElement)
+        [MemberData(nameof(ThreeRootElements))]
+        public void Per_element_xsd_has_no_validation_issues(RootElement rootElement)
         {
             var validationMessages = new List<ValidationEventArgs>();
 
@@ -58,12 +45,10 @@ namespace WarHub.ArmouryModel.Source.Tests
         }
 
         [Theory]
-        [InlineData(XmlInformation.RootElement.Catalogue)]
-        [InlineData(XmlInformation.RootElement.GameSystem)]
-        [InlineData(XmlInformation.RootElement.Roster)]
-        public void Per_element_xsd_contains_Roster_Catalogue_Gamesystem_root_elements(XmlInformation.RootElement rootElement)
+        [MemberData(nameof(ThreeRootElements))]
+        public void Per_element_xsd_contains_all_root_elements(RootElement rootElement)
         {
-            var xmlns = XmlInformation.Namespace(rootElement);
+            var xmlns = Namespace(rootElement);
             var schema = ReadSchema(rootElement, IgnoreEvent);
             schema.TargetNamespace.Should().Be(xmlns);
             //schema.
@@ -77,11 +62,26 @@ namespace WarHub.ArmouryModel.Source.Tests
             void IgnoreEvent(object sender, ValidationEventArgs e) { }
         }
 
+        public static IEnumerable<object[]> XslMigrationVersionData()
+        {
+            return
+                from version in BsDataVersions
+                from element in new[] { RootElement.GameSystem, RootElement.Catalogue }
+                select new object[] { element, version };
+        }
+
+        public static IEnumerable<object[]> ThreeRootElements()
+        {
+            return
+                from element in new[] { RootElement.GameSystem, RootElement.Catalogue, RootElement.Roster }
+                select new object[] { element };
+        }
+
         private static XmlSchema ReadSchema(
-            XmlInformation.RootElement rootElement,
+            RootElement rootElement,
             ValidationEventHandler validationEventHandler)
         {
-            using (var xsdStream = XmlInformation.OpenXsdStream(rootElement))
+            using (var xsdStream = OpenXsdStream(rootElement))
             {
                 var schema = XmlSchema.Read(xsdStream, validationEventHandler);
                 var set = new XmlSchemaSet();
