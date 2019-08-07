02f67856e830c3a0e7f87d77ee2d6f7be8f065b6
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-08-07 22:39:15 +0200

    Test available migrations

--- tests/WarHub.ArmouryModel.Source.BattleScribe.Tests/DataVersionManagementTests.cs
+++ tests/WarHub.ArmouryModel.Source.BattleScribe.Tests/DataVersionManagementTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using FluentAssertions;
@@ -15,13 +16,12 @@ namespace WarHub.ArmouryModel.Source.BattleScribe.Tests
             {
                 var result = DataVersionManagement.ApplyMigrations(gstStream);
 
-                var dataVersion =
-                    XDocument.Load(result)
+                XDocument.Load(result)
                     .Root
                     .Attribute(DataVersionManagement.BattleScribeVersionAttributeName)
                     .Value
-                    .ParseBsDataVersion();
-                dataVersion.Should().Be(XmlInformation.BsDataVersions.Last());
+                    .ParseBsDataVersion()
+                    .Should().Be(XmlInformation.BsDataVersions.Last());
             }
         }
 
@@ -32,14 +32,48 @@ namespace WarHub.ArmouryModel.Source.BattleScribe.Tests
             {
                 var result = DataVersionManagement.ApplyMigrations(gstStream);
 
-                var dataVersion =
-                    XDocument.Load(result)
+                XDocument.Load(result)
                     .Root
                     .Attribute(DataVersionManagement.BattleScribeVersionAttributeName)
                     .Value
-                    .ParseBsDataVersion();
-                dataVersion.Should().Be(XmlInformation.BsDataVersions.Last());
+                    .ParseBsDataVersion()
+                    .Should().Be(XmlInformation.BsDataVersions.Last());
             }
+        }
+
+        [Theory]
+        [MemberData(nameof(HandledMigrationInputs))]
+        public void Migrating_elements_succeeds(XmlInformation.RootElement rootElement, string versionString)
+        {
+            var xmlContent = string.Format(
+                "<{0} {1}='{2}' />",
+                rootElement.Info().XmlElementName,
+                DataVersionManagement.BattleScribeVersionAttributeName,
+                versionString);
+            using (var xmlStream = new MemoryStream())
+            using (var writer = new StreamWriter(xmlStream) { AutoFlush = true })
+            {
+                writer.Write(xmlContent);
+                writer.Flush();
+                xmlStream.Position = 0;
+
+                var result = DataVersionManagement.ApplyMigrations(xmlStream);
+
+                XDocument.Load(result)
+                    .Root
+                    .Attribute(DataVersionManagement.BattleScribeVersionAttributeName)
+                    .Value
+                    .ParseBsDataVersion()
+                    .Should().Be(XmlInformation.BsDataVersions.Last());
+            }
+        }
+
+        public static IEnumerable<object[]> HandledMigrationInputs()
+        {
+            return
+                from root in new[] { XmlInformation.RootElement.GameSystem, XmlInformation.RootElement.Catalogue }
+                from targetVersion in root.Info().AvailableMigrations(XmlInformation.BsDataVersion.v1_15)
+                select new object[] { root, targetVersion.Info().DisplayString };
         }
     }
 }
