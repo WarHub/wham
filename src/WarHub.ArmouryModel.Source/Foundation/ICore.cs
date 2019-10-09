fe1e05ed20cd3d2f7c535bd1a1a8171f6d1d6e0a
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-10-09 18:17:25 +0200

    Some unused code cleanup

--- src/WarHub.ArmouryModel.Source/Foundation/ICore.cs
+++ src/WarHub.ArmouryModel.Source/Foundation/ICore.cs
@@ -3,30 +3,5 @@
     public interface ICore<out TNode> where TNode : SourceNode
     {
         TNode ToNode(SourceNode parent = null);
-
-        // TODO uncomment after generation implemented
-
-        //Type GetSerializationProxyType();
-
-        //Type GetBuilderType();
-
-        //Type GetSerializationEnumerableType();
-    }
-
-    internal struct Witness<TCore, TBuilder, TProxy, TNode>
-        where TCore : NodeCore, ICore<TNode>, IBuildable<TCore, TBuilder>
-        where TBuilder : IBuilder<TCore>
-        where TNode : SourceNode, INodeWithCore<TCore>
-    {
-        public TCore ToCore(TBuilder builder) => builder.ToImmutable();
-
-        public TCore ToCore(TNode node) => ((INodeWithCore<TCore>)node).Core;
-
-        public TNode ToNode(TCore core, SourceNode parent = null)
-            => ((ICore<TNode>)core).ToNode(parent);
-
-        public TBuilder ToBuilder(TCore core) => core.ToBuilder();
-
-        //public TProxy ToProxy(TCore core) => core.ToSerializationProxy();
     }
 }
