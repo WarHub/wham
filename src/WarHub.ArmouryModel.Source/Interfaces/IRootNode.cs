6a72a5d560aa29cc6ad2bc1d9d979594e222f875
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-10-17 15:49:44 +0200

    Make DataIndex implement IRootNode

--- src/WarHub.ArmouryModel.Source/Interfaces/IRootNode.cs
+++ src/WarHub.ArmouryModel.Source/Interfaces/IRootNode.cs
@@ -1,11 +1,13 @@
 ﻿namespace WarHub.ArmouryModel.Source
 {
-    public interface IRootNode : IIdentifiableNode, INameableNode
+    public interface IRootNode : INameableNode
     {
         string BattleScribeVersion { get; }
     }
 
     public partial class CatalogueBaseNode : IRootNode { }
+
+    public partial class DataIndexNode : IRootNode { }
 
     public partial class RosterNode : IRootNode { }
 }
