6cb542fb436a366b7f9971a2c01fb195ead6a2f1
Amadeusz Sadowski <amis.tau.92@gmail.com>
2019-11-02 14:14:18 +0100

    fix: add SEs, SEGs and ELs to EntryLink (#77)
    * fix: add SEs, SEGs and ELs to EntryLink
    
    This is done via changing inheritance: EntryLink now inherits SelectionEntryBase instead of EntryBase.
    
    closes #76
    
    * docs: add changelog entry
    
    * fix: whitespace

--- src/WarHub.ArmouryModel.Source/EntryLinkCore.cs
+++ src/WarHub.ArmouryModel.Source/EntryLinkCore.cs
@@ -5,37 +5,13 @@ namespace WarHub.ArmouryModel.Source
 {
     [WhamNodeCore]
     [XmlType("entryLink")]
-    public partial class EntryLinkCore : EntryBaseCore
+    public partial class EntryLinkCore : SelectionEntryBaseCore
     {
-        [XmlAttribute("collective")]
-        public bool Collective { get; }
-
-        [XmlAttribute("import")]
-        public bool Import { get; }
-
         [XmlAttribute("targetId")]
         public string TargetId { get; }
 
         [XmlAttribute("type")]
         public EntryLinkKind Type { get; }
-
-        [XmlArray("constraints")]
-        public ImmutableArray<ConstraintCore> Constraints { get; }
-
-        [XmlArray("profiles")]
-        public ImmutableArray<ProfileCore> Profiles { get; }
-
-        [XmlArray("rules")]
-        public ImmutableArray<RuleCore> Rules { get; }
-
-        [XmlArray("infoGroups")]
-        public ImmutableArray<InfoGroupCore> InfoGroups { get; }
-
-        [XmlArray("infoLinks")]
-        public ImmutableArray<InfoLinkCore> InfoLinks { get; }
-
-        [XmlArray("categoryLinks")]
-        public ImmutableArray<CategoryLinkCore> CategoryLinks { get; }
 
         [XmlArray("costs")]
         public ImmutableArray<CostCore> Costs { get; }
