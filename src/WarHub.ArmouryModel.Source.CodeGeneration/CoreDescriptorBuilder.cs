f27352d6cb2a999e0e82f7c27fb2be328f5596bc
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-09-28 16:44:30 +0200

    refactor: Replace CG.R with .NET SourceGenerator

--- src/WarHub.ArmouryModel.Source.CodeGeneration/CoreDescriptorBuilder.cs
+++ src/WarHub.ArmouryModel.Source.CodeGeneration/CoreDescriptorBuilder.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Threading;
-using CodeGeneration.Roslyn;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -13,107 +9,22 @@ namespace WarHub.ArmouryModel.Source.CodeGeneration
 {
     internal class CoreDescriptorBuilder
     {
-        private const string ImmutableArrayMetadataName = "System.Collections.Immutable.ImmutableArray`1";
-        private const string WhamNodeCoreAttributeMetadataName = "WarHub.ArmouryModel.Source.WhamNodeCoreAttribute";
-
-        private static SymbolCache<INamedTypeSymbol> immutableArraySymbolCache = new SymbolCache<INamedTypeSymbol>(ImmutableArrayMetadataName);
-        private static SymbolCache<INamedTypeSymbol> whamNodeCoreAttributeSymbolCache = new SymbolCache<INamedTypeSymbol>(WhamNodeCoreAttributeMetadataName);
-
-        public CoreDescriptorBuilder(TransformationContext context, CancellationToken cancellationToken = default)
-        {
-            SemanticModel = context.SemanticModel;
-            Compilation = context.Compilation;
-            TypeDeclaration = (ClassDeclarationSyntax)context.ProcessingNode;
-            CancellationToken = cancellationToken;
-            UpdateNamedTypeSymbolCache(ref immutableArraySymbolCache, Compilation);
-            UpdateNamedTypeSymbolCache(ref whamNodeCoreAttributeSymbolCache, Compilation);
-            TypeSymbol = GetNamedTypeSymbol(TypeDeclaration) ?? throw new ArgumentException("Type is not a named type.");
-        }
-
-        private SemanticModel SemanticModel { get; }
-        private CSharpCompilation Compilation { get; }
-        private ClassDeclarationSyntax TypeDeclaration { get; }
-        private CancellationToken CancellationToken { get; }
-        public static INamedTypeSymbol ImmutableArraySymbol => immutableArraySymbolCache.Symbol!;
-        public INamedTypeSymbol TypeSymbol { get; }
-
-        public CoreDescriptor CreateDescriptor()
-        {
-            var firstDeclaration =
-                NodeFromLocation(TypeSymbol.Locations[0])
-                .FirstAncestorOrSelf<ClassDeclarationSyntax>();
-
-            var attributeLists = GetClassAttributeLists().ToImmutableArray();
-
-            var properties =
-                GetCustomBaseTypesAndSelf(TypeSymbol)
-                .Reverse()
-                .SelectMany(GetAutoReadonlyPropertySymbols)
-                .Select(p =>
-                {
-                    var x = p.DeclaringSyntaxReferences.FirstOrDefault();
-                    var syntax = (PropertyDeclarationSyntax?)x?.GetSyntax();
-                    var auto = syntax?.AccessorList?.Accessors.All(accessor => accessor.Body == null && accessor.ExpressionBody == null) ?? false;
-                    return auto ? new { syntax = syntax!, symbol = p } : null;
-                })
-                .Where(x => x != null)
-                .Select(x => CreateRecordEntry(x!.symbol, x.syntax))
-                .ToImmutableArray();
-
-            return new CoreDescriptor(
-                TypeSymbol,
-                firstDeclaration.GetTypeSyntax().WithoutTrivia(),
-                firstDeclaration.Identifier.WithoutTrivia(),
-                properties,
-                attributeLists);
-        }
-
-        private static IEnumerable<INamedTypeSymbol> GetCustomBaseTypesAndSelf(INamedTypeSymbol self)
-        {
-            yield return self;
-            while (self.BaseType?.SpecialType == SpecialType.None)
-            {
-                self = self.BaseType;
-                yield return self;
-            }
-        }
-
-        private static IEnumerable<IPropertySymbol> GetAutoReadonlyPropertySymbols(INamedTypeSymbol namedTypeSymbol)
-        {
-            return namedTypeSymbol
-                .GetMembers()
-                .OfType<IPropertySymbol>()
-                .Where(p => p.IsReadOnly && !p.IsStatic && !p.IsIndexer && p.DeclaredAccessibility == Accessibility.Public);
-        }
-
-        private static SyntaxNode NodeFromLocation(Location l)
-        {
-            return l.SourceTree.GetRoot().FindNode(l.SourceSpan);
-        }
-
-        private IEnumerable<AttributeListSyntax> GetClassAttributeLists()
-        {
-            var xmlAttributeNames = new[] { Names.XmlType, Names.XmlRoot };
-            var attributes = TypeSymbol.Locations
-                .SelectMany(l => NodeFromLocation(l).FirstAncestorOrSelf<ClassDeclarationSyntax>().AttributeLists)
-                .SelectMany(list => list.Attributes)
-                .ToImmutableArray();
-            var xmlAttributes = attributes.Where(att => xmlAttributeNames.Any(name => att.IsNamed(name)));
-            return xmlAttributes.Select(att => SyntaxFactory.AttributeList().AddAttributes(att));
-        }
-
-        public static CoreDescriptor.Entry CreateRecordEntry(IPropertySymbol symbol, PropertyDeclarationSyntax syntax)
+        public static CoreDescriptor.Entry CreateRecordEntry(
+            IPropertySymbol symbol,
+            PropertyDeclarationSyntax syntax,
+            INamedTypeSymbol immutableArraySymbol,
+            INamedTypeSymbol attributeSymbol)
         {
             var typeSyntax = syntax.Type;
             var typeIdentifier = syntax.Identifier.WithoutTrivia();
             var attributes = GetPropertyAttributeLists(syntax).ToImmutableArray();
             if (symbol.Type is INamedTypeSymbol namedType && namedType.SpecialType == SpecialType.None)
             {
-                if (namedType.Arity == 1 && ImmutableArraySymbol.Equals(namedType.OriginalDefinition, SymbolEqualityComparer.IncludeNullability))
+                if (namedType.Arity == 1 && immutableArraySymbol.Equals(namedType.OriginalDefinition, SymbolEqualityComparer.IncludeNullability))
                 {
                     return new CoreDescriptor.CollectionEntry(symbol, typeIdentifier, (NameSyntax)typeSyntax, attributes);
                 }
-                if (namedType.GetAttributes().Any(a => a.AttributeClass.Equals(whamNodeCoreAttributeSymbolCache.Symbol, SymbolEqualityComparer.Default)))
+                if (namedType.GetAttributes().Any(a => a.AttributeClass?.Equals(attributeSymbol, SymbolEqualityComparer.Default) == true))
                 {
                     return new CoreDescriptor.ComplexEntry(symbol, typeIdentifier, typeSyntax, attributes);
                 }
@@ -127,51 +38,6 @@ namespace WarHub.ArmouryModel.Source.CodeGeneration
             var attributes = syntax.AttributeLists.SelectMany(list => list.Attributes);
             var xmlAttributes = attributes.Where(att => xmlAttributeNames.Any(name => att.IsNamed(name)));
             return xmlAttributes.Select(att => SyntaxFactory.AttributeList().AddAttributes(att));
-        }
-
-        private INamedTypeSymbol? GetNamedTypeSymbol(SyntaxNode node)
-        {
-            var namedTypeSymbol = SemanticModel.GetDeclaredSymbol(node, CancellationToken) as INamedTypeSymbol;
-            return namedTypeSymbol is IErrorTypeSymbol ? null : namedTypeSymbol;
-        }
-
-        private static void UpdateNamedTypeSymbolCache(ref SymbolCache<INamedTypeSymbol> cache, CSharpCompilation compilation)
-        {
-            if (cache.Compilation != compilation)
-            {
-                cache = new SymbolCache<INamedTypeSymbol>(
-                    compilation.GetTypeByMetadataName(cache.FullMetadataName),
-                    compilation,
-                    cache.FullMetadataName);
-            }
-        }
-
-        private readonly struct SymbolCache<T>
-        {
-            public SymbolCache(string fullMetadataName)
-            {
-                FullMetadataName = fullMetadataName;
-                Symbol = default;
-                Compilation = null;
-            }
-
-            public SymbolCache(T symbol, CSharpCompilation compilation, string fullMetadataName)
-            {
-                FullMetadataName = fullMetadataName;
-                Symbol = symbol;
-                Compilation = compilation;
-            }
-
-            [AllowNull, MaybeNull]
-            public T Symbol { get; }
-            public CSharpCompilation? Compilation { get; }
-            public string FullMetadataName { get; }
-
-            public void Deconstruct([MaybeNull] out T symbol, out CSharpCompilation? compilation)
-            {
-                symbol = Symbol;
-                compilation = Compilation;
-            }
         }
     }
 }
--- src/WarHub.ArmouryModel.Source.CodeGeneration/RoslynWhamNodeGenerator.cs
+++ src/WarHub.ArmouryModel.Source.CodeGeneration/RoslynWhamNodeGenerator.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
+using MoreLinq;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace WarHub.ArmouryModel.Source.CodeGeneration
@@ -13,6 +14,9 @@ namespace WarHub.ArmouryModel.Source.CodeGeneration
     [Generator]
     public class RoslynWhamNodeGenerator : ISourceGenerator
     {
+        private const string ImmutableArrayMetadataName = "System.Collections.Immutable.ImmutableArray`1";
+        private const string WhamNodeCoreAttributeMetadataName = "WarHub.ArmouryModel.Source.WhamNodeCoreAttribute";
+
         public void Initialize(GeneratorInitializationContext context)
         {
             context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
@@ -20,96 +24,149 @@ namespace WarHub.ArmouryModel.Source.CodeGeneration
 
         public void Execute(GeneratorExecutionContext context)
         {
+            //Debugger.Launch();
             if (context.SyntaxReceiver is not SyntaxReceiver syntaxReceiver)
                 return;
 
+            var parseOptions = context.ParseOptions;
+            var attributeSymbol = context.Compilation.GetTypeByMetadataName(WhamNodeCoreAttributeMetadataName)
+                ?? throw new InvalidOperationException("Symbol not found: " + WhamNodeCoreAttributeMetadataName);
+            var immutableArraySymbol = context.Compilation.GetTypeByMetadataName(ImmutableArrayMetadataName)
+                ?? throw new InvalidOperationException("Symbol not found: " + ImmutableArrayMetadataName);
             var coreSymbols = GetNodeCoreSymbols().ToHashSet();
 
             foreach (var coreSymbol in coreSymbols)
             {
                 var descriptor = CreateDescriptor(coreSymbol);
-                var text = CreateSourceText(descriptor);
-                context.AddSource(descriptor.RawModelName, text);
+                var compilationUnit = CreateSource(descriptor);
+                var sourceText = SyntaxTree(compilationUnit, parseOptions, encoding: Encoding.UTF8).GetText();
+                context.AddSource(descriptor.RawModelName, sourceText);
             }
 
             IEnumerable<INamedTypeSymbol> GetNodeCoreSymbols()
             {
-                var attributeSymbol = context.Compilation.GetTypeByMetadataName("WarHub.ArmouryModel.Source.WhamNodeCoreAttribute");
                 foreach (var candidate in syntaxReceiver.CandidateClasses)
                 {
                     var model = context.Compilation.GetSemanticModel(candidate.SyntaxTree);
                     var classSymbol = model.GetDeclaredSymbol(candidate);
-                    if (classSymbol.GetAttributes().Any(data => data.AttributeClass.Equals(attributeSymbol, SymbolEqualityComparer.Default)))
+                    if (classSymbol?.GetAttributes().Any(data => data.AttributeClass?.Equals(attributeSymbol, SymbolEqualityComparer.Default) == true) == true)
                     {
                         yield return classSymbol;
                     }
                 }
             }
-        }
 
-        private static CoreDescriptor CreateDescriptor(INamedTypeSymbol coreSymbol)
-        {
-            var declarationSyntax = (ClassDeclarationSyntax)coreSymbol.DeclaringSyntaxReferences[0].GetSyntax();
-            var attributes = declarationSyntax.AttributeLists
-                .SelectMany(x => x.Attributes)
-                .Where(x => x.IsNamed(Names.XmlRoot) || x.IsNamed(Names.XmlType))
-                .Select(x => AttributeList().AddAttributes(x))
-                .ToImmutableArray();
-
-            var entries = GetCustomBaseTypesAndSelf(coreSymbol)
-                .Reverse()
-                .SelectMany(x => x.GetMembers().OfType<IPropertySymbol>())
-                .Where(p => p is { IsReadOnly: true, IsStatic: false, IsIndexer: false, DeclaredAccessibility: Accessibility.Public })
-                .Select(p =>
-                {
-                    var x = p.DeclaringSyntaxReferences.FirstOrDefault();
-                    var syntax = (PropertyDeclarationSyntax?)x?.GetSyntax();
-                    var auto = syntax?.AccessorList?.Accessors.All(accessor => accessor.Body == null && accessor.ExpressionBody == null) ?? false;
-                    return auto ? new { syntax = syntax!, symbol = p } : null;
-                })
-                .Where(x => x != null)
-                .Select(x => CoreDescriptorBuilder.CreateRecordEntry(x!.symbol, x.syntax))
-                .ToImmutableArray();
-            var descriptor = new CoreDescriptor(
-                coreSymbol,
-                ParseName(coreSymbol.ToDisplayString()),
-                declarationSyntax.Identifier.WithoutTrivia(),
-                entries,
-                attributes);
-            return descriptor;
-
-            static IEnumerable<INamedTypeSymbol> GetCustomBaseTypesAndSelf(INamedTypeSymbol self)
+            CoreDescriptor CreateDescriptor(INamedTypeSymbol coreSymbol)
             {
-                yield return self;
-                while (self.BaseType?.SpecialType == SpecialType.None)
+                var declarationSyntax = (ClassDeclarationSyntax)coreSymbol.DeclaringSyntaxReferences[0].GetSyntax();
+                var attributes = declarationSyntax.AttributeLists
+                    .SelectMany(x => x.Attributes)
+                    .Where(x => x.IsNamed(Names.XmlRoot) || x.IsNamed(Names.XmlType))
+                    .Select(x => AttributeList().AddAttributes(x))
+                    .ToImmutableArray();
+
+                var entries = GetCustomBaseTypesAndSelf(coreSymbol)
+                    .Reverse()
+                    .SelectMany(x => x.GetMembers().OfType<IPropertySymbol>())
+                    .Where(p => p is { IsReadOnly: true, IsStatic: false, IsIndexer: false, DeclaredAccessibility: Accessibility.Public })
+                    .Select(p =>
+                    {
+                        var x = p.DeclaringSyntaxReferences.FirstOrDefault();
+                        var syntax = (PropertyDeclarationSyntax?)x?.GetSyntax();
+                        var auto = syntax?.AccessorList?.Accessors.All(accessor => accessor.Body == null && accessor.ExpressionBody == null) ?? false;
+                        return auto ? new { syntax = syntax!, symbol = p } : null;
+                    })
+                    .Where(x => x != null)
+                    .Select(x => CoreDescriptorBuilder.CreateRecordEntry(x!.symbol, x.syntax, immutableArraySymbol, attributeSymbol))
+                    .ToImmutableArray();
+                var descriptor = new CoreDescriptor(
+                    coreSymbol,
+                    ParseName(coreSymbol.ToDisplayString()),
+                    declarationSyntax.Identifier.WithoutTrivia(),
+                    entries,
+                    attributes);
+                return descriptor;
+
+                static IEnumerable<INamedTypeSymbol> GetCustomBaseTypesAndSelf(INamedTypeSymbol self)
                 {
-                    self = self.BaseType;
                     yield return self;
+                    while (self.BaseType?.SpecialType == SpecialType.None)
+                    {
+                        self = self.BaseType;
+                        yield return self;
+                    }
                 }
             }
         }
 
-        private static SourceText CreateSourceText(CoreDescriptor descriptor)
+        private static CompilationUnitSyntax CreateSource(CoreDescriptor descriptor)
         {
+            var declaredUsings = descriptor.TypeSymbol.DeclaringSyntaxReferences[0].GetSyntax().SyntaxTree.GetCompilationUnitRoot().Usings;
+            var usings = RequiredUsings.AddRange(
+                declaredUsings.Where(x => !RequiredNamespaces.Contains(x.Name.ToString())));
             var generatedMembers = List<MemberDeclarationSyntax>();
             generatedMembers = generatedMembers.AddRange(GenerateCorePartials(descriptor));
             generatedMembers = generatedMembers.AddRange(GenerateNodePartials(descriptor));
             return
                CompilationUnit()
-               .AddUsings(
-                   UsingDirective(ParseName(Names.NamespaceSystem)),
-                   UsingDirective(ParseName(Names.NamespaceSystemCollectionsGeneric)),
-                   UsingDirective(ParseName(Names.NamespaceSystemCollectionsImmutable)),
-                   UsingDirective(ParseName(Names.NamespaceSystemDiagnostics)),
-                   UsingDirective(ParseName(Names.NamespaceSystemDiagnosticsCodeAnalysis)),
-                   UsingDirective(ParseName(Names.NamespaceSystemXmlSerialization)))
+               .AddUsings(usings.ToArray())
                .AddMembers(
                    NamespaceDeclaration(
                        ParseName(descriptor.TypeSymbol.ContainingNamespace.ToDisplayString()))
                    .WithMembers(generatedMembers))
-               .SyntaxTree
-               .GetText();
+               .WithLeadingTrivia(
+                   TriviaList(PragmaWarningDisable))
+               .NormalizeWhitespace();
         }
+
+        private static SyntaxTrivia? pragmaWarningDisable;
+        private static SyntaxTrivia PragmaWarningDisable =>
+            pragmaWarningDisable ??= CreatePragmaWarningDisable();
+
+        private static SyntaxTrivia CreatePragmaWarningDisable()
+        {
+            var codes = new[]
+            {
+                "CA1034", // Nested types should not be visible
+                "CA1054", // URI parameters should not be strings
+                "CA1056", // URI properties should not be strings
+                "CA1062", // Validate arguments of public methods
+                "CA1710", // Identifiers should have correct suffix
+                "CA1716", // Identifiers should not match keywords
+                "CA1815", // Override equals and operator equals on value types
+                "CA2225", // Operator overloads have named alternates
+                "CA2227", // Collection properties should be read only
+            };
+            return
+                Trivia(
+                    PragmaWarningDirectiveTrivia(
+                        Token(SyntaxKind.DisableKeyword),
+                        SeparatedList<ExpressionSyntax>(
+                            codes.Select(x => IdentifierName(x))),
+                        isActive: true));
+        }
+
+        private static ImmutableHashSet<string> RequiredNamespaces { get; } =
+            ImmutableHashSet.Create(
+                Names.NamespaceSystem,
+                Names.NamespaceSystemCollectionsGeneric,
+                Names.NamespaceSystemCollectionsImmutable,
+                Names.NamespaceSystemDiagnostics,
+                Names.NamespaceSystemDiagnosticsCodeAnalysis,
+                Names.NamespaceSystemXmlSerialization);
+
+        private static ImmutableArray<UsingDirectiveSyntax>? requiredUsings;
+
+        private static ImmutableArray<UsingDirectiveSyntax> RequiredUsings => requiredUsings ??= CreateRequiredUsings();
+
+        private static ImmutableArray<UsingDirectiveSyntax> CreateRequiredUsings() =>
+            ImmutableArray.Create(
+                UsingDirective(ParseName(Names.NamespaceSystem)),
+                UsingDirective(ParseName(Names.NamespaceSystemCollectionsGeneric)),
+                UsingDirective(ParseName(Names.NamespaceSystemCollectionsImmutable)),
+                UsingDirective(ParseName(Names.NamespaceSystemDiagnostics)),
+                UsingDirective(ParseName(Names.NamespaceSystemDiagnosticsCodeAnalysis)),
+                UsingDirective(ParseName(Names.NamespaceSystemXmlSerialization)));
 
         private static IEnumerable<TypeDeclarationSyntax> GenerateCorePartials(CoreDescriptor descriptor)
         {
