02b598a5d5a4fb7a44015b0a3364fec8e6602e70
Amadeusz Sadowski <amis.tau.92@gmail.com>
2020-09-28 16:49:11 +0200

    refactor: Cleanup CG.R references

--- src/WarHub.ArmouryModel.Source.CodeGeneration.Attributes/WhamNodeCoreAttribute.cs
+++ src/WarHub.ArmouryModel.Source.CodeGeneration.Attributes/WhamNodeCoreAttribute.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
-using CodeGeneration.Roslyn;
 
 namespace WarHub.ArmouryModel.Source
 {
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-    [CodeGenerationAttribute("WarHub.ArmouryModel.Source.CodeGeneration.WhamNodeGenerator, WarHub.ArmouryModel.Source.CodeGeneration")]
     [Conditional("CodeGeneration")]
     public sealed class WhamNodeCoreAttribute : Attribute
     {
